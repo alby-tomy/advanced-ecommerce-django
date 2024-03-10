d46e7141fc3702b8870e5688eb0abd2169a8bf3d
alby-tomy <alby.u.tomy@gmail.com>
2024-03-10 15:37:49 +0530

    model creation category and product

--- ecommerce_project/store/admin.py
+++ ecommerce_project/store/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','category','original_price','selling_price']
+    list_editable = ['original_price','selling_price']
+    prepopulated_fields = {'slug':('name',)}
+    list_per_page = 15
+    
+admin.site.register(Product, ProductAdmin)
--- ecommerce_project/store/models.py
+++ ecommerce_project/store/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='categories/')  # Assuming you have an "categories" directory in your MEDIA_ROOT
+    status = models.BooleanField(default=True)  # Checkbox for status
+    trending = models.BooleanField(default=False)  # Checkbox for trending
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        
+    def __str__(self):
+        return self.name
+    
+    def get_url(self):
+        return reverse('e_commerceApp:products_by_cat', args=[self.slug])
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='products/')  # Assuming you have an "products" directory in your MEDIA_ROOT
+    description = models.TextField()
+    quantity = models.IntegerField(default=0)
+    original_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)  # Checkbox for status
+    trending = models.BooleanField(default=False)  # Checkbox for trending
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+        
+    def __str__(self):
+        return self.name
+    
+    def get_url(self):
+        return reverse('e_commerceApp:proDetailName', args=[self.category.slug,self.slug])
